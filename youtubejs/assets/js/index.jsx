47ce258d272e6674735decb8fd0c20cf48f0a84f
jony37 <156230610+jony37@users.noreply.github.com>
2024-06-24 19:43:40 +0500

    Add files via upload

---
+++ youtubejs/assets/js/index.jsx
@@ -0,0 +1,17 @@
+const changeNavMenu = () => {
+    const navMenu = document.getElementsByClassName('onclik');
+    console.log('salomnjejc');
+}
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', changeNavMenu);
+
+
+// const changeHeading = () => {
+//     const h1 = document.getElementById('heading');
+//     h1.textContent = 'New Jahon';
+//     h1.setAttribute('style', 'color:red; transition: all 1s;')
+// }
+
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', changeHeading);
